a68386e34603cbd4a15b50b47080f4a9fbd6e591
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-11-17 15:32:13 -0300

    incluindo Itarefas e props

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import Button from "../Button";
 import style from "./Formulario.module.scss";
+import { Itarefa } from "../../types/tarefas";
 
+const tarefaInicial: Itarefa = {
+  tarefa: '',
+  tempo: '00:00'
+}
 
-function Formulario() {
-  const [tempo, setTempo] = useState("00:00");
-  const [tarefa, setTarefa] = useState("");
+function Formulario({setTarefas}: {setTarefas:React.Dispatch<React.SetStateAction<Itarefa[]>>}) {
+  const [tarefa, setTarefa] = useState<Itarefa>(tarefaInicial);
 
   function adicionarTarefa(evento: React.FormEvent) {
     evento.preventDefault();
-    console.log()
+    setTarefas((tarefasAntigas) => [...tarefasAntigas, tarefa])
+    setTarefa(tarefaInicial)
   }
 
   return (
@@ -18,15 +23,15 @@ function Formulario() {
         <label htmlFor="tarefa">
           Adicione um novo estudo
         </label>
-        <input type="text" value={tarefa} onChange={evento => setTarefa(evento.target.value)} name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
+        <input type="text" value={tarefa.tarefa} onChange={evento => setTarefa(tarefasAntigas => ({ ...tarefasAntigas, tarefa: evento.target.value }))} name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
       </div>
       <div className={style.inputContainer}>
         <label htmlFor="tempo">
           tempo
         </label>
-        <input type="time" value={tempo} onChange={evento => setTempo(evento.target.value)} step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required />
+        <input type="time" value={tarefa.tempo} onChange={evento => setTarefa(tarefasAntigas => ({ ...tarefasAntigas, tempo: evento.target.value }))} step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required />
       </div>
-      <Button>Adicionar</Button>
+      <Button type="submit">Adicionar</Button>
     </form>
   );
 }
