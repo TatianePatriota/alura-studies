c558af1325934d27eb52e43d0f9210dfe695c8ef
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-11-17 15:31:27 -0300

    editando lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 import Item from "./Item";
 import style from "./Lista.module.scss";
+import { Itarefa } from "../../types/tarefas";
 
-function Lista() {
-  const [tarefas, setTarefas] = useState([{
-    tarefa: "React",
-    tempo:"02:00:00"
-  },
-  {
-    tarefa: "Javascript",
-    tempo: "01:00:00"
-  },
-  {
-    tarefa: "Typescript",
-    tempo: "03:00:00"
-  }]);
+function Lista({tarefas}: {tarefas:Itarefa[]}) {
   return(
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {setTarefas([...tarefas, {tarefa:"estudar estado", tempo: "02:00:00"}])}}>Estudos do dia</h2>
+      <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index)=> (
           <Item key={index} {...item}/>
