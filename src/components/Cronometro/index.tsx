8595e152b60fd63e63a76e67c2e22886a5bbad1d
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-11-18 07:37:42 -0300

    add useEffect para tarefa selecionada

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,13 +1,27 @@
 import Button from "../Button";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
-import tempoParaSegundos from "../../common/utils/date";
+import tempoParaSegundos from "../../common/utils/time";
+import { Itarefa } from "../../types/tarefas";
+import { useEffect, useState } from "react";
 
-function Cronometro() {
-  console.log("conversão:", tempoParaSegundos("01:01:01"));
+interface Props {
+  selecionado: Itarefa | undefined
+}
+
+function Cronometro({selecionado}: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if(selecionado?.tempo){
+      setTempo(tempoParaSegundos(selecionado?.tempo))
+    }
+  },[selecionado])
+ 
   return(
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo:{tempo}
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
