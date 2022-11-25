08ea9317bd9d37233e5e15afec4767be1772cd50
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-11-25 16:20:35 -0300

    add ternario para finalizar tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,22 +1,23 @@
 import { Itarefa } from "../../../types/tarefas";
 import style from "./Item.module.scss";
 
-interface Props extends Itarefa{
-  selecionaTarefa: (tarefaSelecionada:Itarefa) => void
+interface Props extends Itarefa {
+  selecionaTarefa: (tarefaSelecionada: Itarefa) => void
 }
 
 export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
   return (
-    <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-      onClick={() => selecionaTarefa({
-      tarefa,
-      tempo,
-      selecionado,
-      completado,
-      id
-    })}>
+    <li className={`${style.item} ${selecionado ? style.itemSelecionado : ""} ${completado ? style.itemCompletado : ""}`}
+      onClick={() => !completado && selecionaTarefa({
+        tarefa,
+        tempo,
+        selecionado,
+        completado,
+        id
+      })}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado && <span className={style.concluido} aria-label="tarefa completada"></span>}
     </li>
   );
 }
