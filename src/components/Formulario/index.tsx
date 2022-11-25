313a7eecfe65f640e801874b81c8fd413fdf4252
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-11-25 16:25:25 -0300

    alterando title do  form

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 import Button from "../Button";
 import style from "./Formulario.module.scss";
 import { Itarefa } from "../../types/tarefas";
-import { v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 const tarefaInicial = (): Itarefa => ({
   tarefa: '',
   tempo: '00:00',
   selecionado: false,
-  completado:false,
+  completado: false,
   id: uuidv4()
 })
 
-function Formulario({setTarefas}: {setTarefas:React.Dispatch<React.SetStateAction<Itarefa[]>>}) {
+function Formulario({ setTarefas }: { setTarefas: React.Dispatch<React.SetStateAction<Itarefa[]>> }) {
   const [tarefa, setTarefa] = useState<Itarefa>(() => tarefaInicial());
 
-  function adicionarTarefa(evento: React.FormEvent) { 
+  function adicionarTarefa(evento: React.FormEvent) {
     evento.preventDefault();
-  setTarefas(tarefasAntigas => [...tarefasAntigas, tarefa])
+    setTarefas(tarefasAntigas => [...tarefasAntigas, tarefa])
     setTarefa(tarefaInicial())
   }
 
@@ -25,13 +25,13 @@ function Formulario({setTarefas}: {setTarefas:React.Dispatch<React.SetStateActio
     <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
       <div className={style.inputContainer}>
         <label htmlFor="tarefa">
-          Adicione um novo estudo
+          Adicionar novo estudo
         </label>
         <input type="text" value={tarefa.tarefa} onChange={evento => setTarefa(tarefasAntigas => ({ ...tarefasAntigas, tarefa: evento.target.value }))} name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
       </div>
       <div className={style.inputContainer}>
         <label htmlFor="tempo">
-          tempo
+          Tempo
         </label>
         <input type="time" value={tarefa.tempo} onChange={evento => setTarefa(tarefasAntigas => ({ ...tarefasAntigas, tempo: evento.target.value }))} step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required />
       </div>
